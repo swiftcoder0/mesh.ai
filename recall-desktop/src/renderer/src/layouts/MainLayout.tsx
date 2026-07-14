95fcf38791a71027ae8b22832d52aab64940ec34
swiftcoder <pandayaditya625@gmail.com>
2026-07-15 02:14:07 +0530

    created application layout

--- recall-desktop/src/renderer/src/layouts/MainLayout.tsx
+++ recall-desktop/src/renderer/src/layouts/MainLayout.tsx
@@ -6,25 +6,26 @@ import StatusBar from "../components/StatusBar";
 
 function MainLayout(): React.JSX.Element {
   return (
-    <div>
+    <div className="h-screen flex flex-col bg-slate-100">
       <Header />
 
-      <main
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          padding: "20px",
-          gap: "20px",
-        }}
-      >
-        <Sidebar />
+      <main className="flex flex-1 overflow-hidden">
+        <aside className="w-64 border-r bg-white p-4">
+          <Sidebar />
+        </aside>
 
-        <PDFViewer />
+        <section className="flex-1 bg-slate-50 p-6">
+          <PDFViewer />
+        </section>
 
-        <RightPanel />
+        <aside className="w-80 border-l bg-white p-4">
+          <RightPanel />
+        </aside>
       </main>
 
-      <StatusBar />
+      <footer className="border-t bg-white px-4 py-2">
+        <StatusBar />
+      </footer>
     </div>
   );
 }
